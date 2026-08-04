15be49a891f79c66afcd5aab49ee2c51b5019a4f
Dilushgaran S <diludilush963@gmail.com>
2026-08-04 14:47:51 +0530

    Remove 'Experience' link from Navbar
    Comment out the 'Experience' link in the Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
         { name: 'About', link: '#about' },
         { name: 'Skills', link: '#skills' },
         { name: 'Projects', link: '#projects' },
-        { name: 'Experience', link: '#experience' },
+        {/*{ name: 'Experience', link: '#experience' },*/}
         { name: 'Contact', link: '#contact' }
     ];
 
